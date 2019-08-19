6d8dfafdf2048737d16764ce582be506f8443d24
Hamed ZVand <sunrise.freedom@gmail.com>
2019-08-19 10:13:04 +0430

    Init project

---
+++ src/Alamut.Abstractions/Caching/ICacheable.cs
@@ -0,0 +1,12 @@
+﻿namespace Alamut.Abstractions.Caching
+{
+    /// <summary>
+    /// apply to a Request in a Messaging systems like MediatR
+    /// and cache the Response of the that Request by provided information 
+    /// </summary>
+    public interface ICacheable
+    {
+        string Key { get; }
+        ExpirationOptions Options { get; }
+    }
+}
---
+++ src/Alamut.Abstractions/Principal/IUserInfo.cs
@@ -0,0 +1,36 @@
+namespace Alamut.Abstractions.Principal
+{
+    public interface IUserInfo
+    {
+        /// <summary>
+        /// gets current user Username
+        /// </summary>
+        string UserName { get; }
+
+        /// <summary>
+        /// gets current user full name (first name + family)
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// gets current user friendly name (fore name)
+        /// if it doesn't represented return userName
+        /// </summary>
+        string GivenName { get; }
+
+        /// <summary>
+        /// gets current user UserId
+        /// </summary>
+        string UserId { get; }
+
+        /// <summary>
+        /// gets current user Ip Address
+        /// </summary>
+        string GetClientIpAddress(bool tryUseXForwardHeader = true);
+
+        /// <summary>
+        /// get current user browser information
+        /// </summary>
+        string UserAgent {get;}
+    }
+}
