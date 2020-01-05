c2b9f2a585416cb3aa1b815d55a6a8fbab667b65
Hamed ZVand <sunrise.freedom@gmail.com>
2020-01-05 09:38:36 +0330

    message handler documentation

--- src/Alamut.Abstractions/Messaging/IMessageHandler[TMessage].cs
+++ src/Alamut.Abstractions/Messaging/IMessageHandler[TMessage].cs
@@ -3,8 +3,21 @@ using System.Threading.Tasks;
 
 namespace Alamut.Abstractions.Messaging
 {
+    /// <summary>
+    /// message handler base interface
+    /// </summary>
+    /// <typeparam name="TMessage"></typeparam>
+    /// <remark>inherited class should decorated with TopicsAttribute</remark>
     public interface IMessageHandler<TMessage>
     {
         Task Handle(TMessage message, CancellationToken token);
+    }
+
+    /// <summary>
+    /// a flag interface to mark Message Handler
+    /// </summary>
+    public interface IMessageHandler
+    {
+        
     }
 }
