908abc13036890c78ff401ba6ae2cdb1d5119662
Hamed ZVand <sunrise.freedom@gmail.com>
2020-01-13 09:53:01 +0330

    add async methods to publisher

--- src/Alamut.Abstractions/Messaging/IPublisher.cs
+++ src/Alamut.Abstractions/Messaging/IPublisher.cs
@@ -14,7 +14,12 @@ namespace Alamut.Abstractions.Messaging
         /// <param name="topic"></param>
         /// <param name="message"></param>
         /// <returns></returns>
-        Task Publish(string topic, string message);
+        /// <remarks>
+        /// Note: Awaiting the asynchronous produce request below prevents flow of execution
+        /// from proceeding until the acknowledgement from the broker is received (at the 
+        /// expense of low throughput).
+        /// </remarks>
+        Task PublishAsync(string topic, string message);
         
         /// <summary>
         /// publish given message to specified topic
@@ -23,7 +28,12 @@ namespace Alamut.Abstractions.Messaging
         /// <param name="topic"></param>
         /// <param name="message"></param>
         /// <returns></returns>
-        Task Publish(string topic, object message);
+        /// <remarks>
+        /// Note: Awaiting the asynchronous produce request below prevents flow of execution
+        /// from proceeding until the acknowledgement from the broker is received (at the 
+        /// expense of low throughput).
+        /// </remarks>
+        Task PublishAsync(string topic, object message);
         
         /// <summary>
         /// publish given message to specified topic
@@ -33,6 +43,38 @@ namespace Alamut.Abstractions.Messaging
         /// <param name="topic"></param>
         /// <param name="message"></param>
         /// <returns></returns>
-        Task Publish(string topic, IMessage message);
+        /// <remarks>
+        /// Note: Awaiting the asynchronous produce request below prevents flow of execution
+        /// from proceeding until the acknowledgement from the broker is received (at the 
+        /// expense of low throughput).
+        /// </remarks>
+        Task PublishAsync(string topic, IMessage message);
+
+        /// <summary>
+        /// publish given message to specified topic
+        /// </summary>
+        /// <param name="topic"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        void Publish(string topic, string message);
+        
+        /// <summary>
+        /// publish given message to specified topic
+        /// serialize message to JSON (with default serializer provider)
+        /// </summary>
+        /// <param name="topic"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        void Publish(string topic, object message);
+        
+        /// <summary>
+        /// publish given message to specified topic
+        /// if IMessage.Id is not provided, generate a new unique Id for it
+        /// serialize message to JSON (with default serializer provider)
+        /// </summary>
+        /// <param name="topic"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        void Publish(string topic, IMessage message);
     }
 }
