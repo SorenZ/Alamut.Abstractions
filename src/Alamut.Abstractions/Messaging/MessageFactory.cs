4b818bb73a6017d322692f36ee857d2aafb97f18
Hamed ZVand <sunrise.freedom@gmail.com>
2020-03-01 10:40:48 +0330

    messageFactory build Message with provided Id

--- src/Alamut.Abstractions/Messaging/MessageFactory.cs
+++ src/Alamut.Abstractions/Messaging/MessageFactory.cs
@@ -20,6 +20,19 @@ namespace Alamut.Abstractions.Messaging
             new Message<T> { Body = body };
 
         /// <summary>
+        /// build a very basic Message provides Id and Body of actual Data
+        /// </summary>
+        /// <param name="body">The actual data that moves through messaging system</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Message<T> Build<T>(string id, T body) =>
+            new Message<T>
+            {
+                Id = id,
+                Body = body
+            };
+
+        /// <summary>
         /// build a Message-with-Acknowledge 
         /// </summary>
         /// <param name="body"></param>
@@ -48,7 +61,7 @@ namespace Alamut.Abstractions.Messaging
             bool acknowledgeRequested = false,
             string acknowledgeTopic = null)
         {
-            if(acknowledgeRequested == true && string.IsNullOrEmpty(acknowledgeTopic))
+            if (acknowledgeRequested == true && string.IsNullOrEmpty(acknowledgeTopic))
             { throw new ArgumentNullException("when `acknowledgeRequested` you must provide `acknowledgeTopic`"); }
 
             return new FullMessage<T>
