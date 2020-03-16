3e394a77b332cbec098d5268bdffd9b53e0bf65f
Hamed ZVand <sunrise.freedom@gmail.com>
2020-03-16 14:51:24 +0330

    implement Enumeration utility in SeedWork\

---
+++ src/Alamut.Abstractions/SeedWork/Enumeration.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Alamut.Abstractions.SeedWork
+{
+    /// <summary>
+    /// Enumerations (or enum types for short) are a thin language wrapper around an integral type.
+    /// Enumeration classes enable all the rich features of an object-oriented language, instead of enum type
+    /// </summary>
+    /// <remarks>
+    /// https://docs.microsoft.com/en-us/dotnet/architecture/microservices/microservice-ddd-cqrs-patterns/enumeration-classes-over-enum-types
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// public class CardType : Enumeration
+    /// {
+    ///     public static readonly CardType Amex = new CardType(1, "Amex");
+    ///     public static readonly CardType Visa = new CardType(2, "Visa");
+    ///     public static readonly CardType MasterCard = new CardType(3, "MasterCard");
+    /// 
+    ///     public CardType(int id, string name)
+    ///         : base(id, name)
+    ///     {
+    ///     }
+    /// }
+    /// </code>
+    /// </example>
+    public abstract class Enumeration : IComparable
+    {
+        public string Name { get; private set; }
+
+        public int Id { get; private set; }
+
+        protected Enumeration(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString() => Name;
+
+        public static IEnumerable<T> GetAll<T>() where T : Enumeration
+        {
+            var fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+
+            return fields.Select(f => f.GetValue(null)).Cast<T>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherValue = obj as Enumeration;
+
+            if (otherValue == null)
+                return false;
+
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Id.Equals(otherValue.Id);
+
+            return typeMatches && valueMatches;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
+
+        public static int AbsoluteDifference(Enumeration firstValue, Enumeration secondValue)
+        {
+            var absoluteDifference = Math.Abs(firstValue.Id - secondValue.Id);
+            return absoluteDifference;
+        }
+
+        public static T FromValue<T>(int value) where T : Enumeration
+        {
+            var matchingItem = Parse<T, int>(value, "value", item => item.Id == value);
+            return matchingItem;
+        }
+
+        public static T FromDisplayName<T>(string displayName) where T : Enumeration
+        {
+            var matchingItem = Parse<T, string>(displayName, "display name", item => item.Name == displayName);
+            return matchingItem;
+        }
+
+        private static T Parse<T, K>(K value, string description, Func<T, bool> predicate) where T : Enumeration
+        {
+            var matchingItem = GetAll<T>().FirstOrDefault(predicate);
+
+            if (matchingItem == null)
+                throw new InvalidOperationException($"'{value}' is not a valid {description} in {typeof(T)}");
+
+            return matchingItem;
+        }
+
+        public int CompareTo(object other) => Id.CompareTo(((Enumeration)other).Id);
+    }
+}
